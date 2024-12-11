2d4de68539000c33c38e5f999f6ac6ef683aad0a
joaovtfor <joaovtfor@hotmail.com>
2024-12-11 09:10:38 -0300

    `Add` - index.tsx init

---
+++ Nearby/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Index = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+        </View>
+    )
+}
+
+export default Index
