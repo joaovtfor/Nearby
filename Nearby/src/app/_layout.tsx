7c14af9b0c331668943af7762e9290691c32fffe
joaovtfor <joaovtfor@hotmail.com>
2024-12-11 11:36:01 -0300

    `Add` - Routing + Loader component

--- Nearby/src/app/_layout.tsx
+++ Nearby/src/app/_layout.tsx
@@ -1,12 +1,34 @@
 import { Stack } from "expo-router"
 import { colors } from "@/styles/colors"
+import {
+    useFonts,
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+} from "@expo-google-fonts/rubik"
+import { Loading } from "@/components/Loading";
 
 const Layout = () => {
+    const [fontsLoaded] =
+        useFonts({
+            Rubik_600SemiBold,
+            Rubik_400Regular,
+            Rubik_500Medium,
+            Rubik_700Bold
+        });
+
+    if (!fontsLoaded) {
+        return <Loading />
+    };
+
     return (
-        <Stack screenOptions={{headerShown: false, 
+        <Stack screenOptions={{
+            headerShown: false,
             contentStyle: {
                 backgroundColor: colors.gray[100],
-            }}} />
+            }
+        }} />
     )
 }
 
